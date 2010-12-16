bb93a1765311697797fab701cf6f0ce1eb1f9cc1
Nijiko Yonskai <nijikokun@gmail.com>
2010-12-15 23:17:47 -0500

    Added license to all files.

--- HTTP.java
+++ HTTP.java
@@ -4,6 +4,24 @@ import java.net.URL;
 import java.net.URLConnection;
 
 /**
+ * iConomy v1.x
+ * Copyright (C) 2010  Nijikokun <nijikokun@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
  * HTTP.java
  * <br /><br />
  * Currently controls any outside url connections. Currently it only checks for the latest version.
--- Messaging.java
+++ Messaging.java
@@ -1,4 +1,22 @@
 /**
+ * iConomy v1.x
+ * Copyright (C) 2010  Nijikokun <nijikokun@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
  * Messaging.java
  * <br /><br />
  * Lets us do fancy pantsy things with colors, messages, and broadcasting :D!
