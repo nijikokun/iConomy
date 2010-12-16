57befc961d5bf6fd24c5545646b0a83ab1aa3313
Nijiko Yonskai <nijikokun@gmail.com>
2010-12-15 23:19:12 -0500

    Forgot JSON file

--- JSON.java
+++ JSON.java
@@ -1,8 +1,25 @@
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+/**
+ * iConomy v1.x
+ * Copyright (C) 2010  Nijikokun <nijikokun@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /**
  * JSON
